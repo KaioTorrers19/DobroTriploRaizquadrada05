67f96560436583b3ab128a72b2f04007ac9f3d9a
Kaio Torres <57267578+KaioTorrers19@users.noreply.github.com>
2021-12-01 13:39:11 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n1 = int(input('digite um numero'))
+n2 = n1*2
+n3 = n1*3
+n4 = n1;(1/2)
+print('o dobro do numero {}! \n dará {}! \n e o triplo o valor {}! \n e a raiz quadrada dara {}!'.format(n1,n2,n3,n4))
